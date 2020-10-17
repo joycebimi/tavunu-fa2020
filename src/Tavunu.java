0a457e2a066c216eb967d970d3a39cab549e4eb7
joyce Bimi <jbimib@gmail.com>
2020-10-16 23:56:10 -0500

    new src

--- src/Tavunu.java
+++ src/Tavunu.java
@@ -9,6 +9,62 @@
  * @author Mithat Konar
  * @author Your Name
  */
+
 public class Tavunu {
-    /* See readme.md for what to do. */
+    String name;
+    int pava
+    int birthYear;     
+    public Tavunu(string name, int birthYear, int pava){
+         this.name = name;
+         this.pava = pava;
+         this.birthYear = birthYear;
+    }
+    
+     public boolean setName(String name) {
+        if (name.charAt(0) != 'T' && name.charAt(0) != 'D') {
+            return false;
+        } else {
+            this.name = name;
+            return true;
+
+        }
+    public String getName(){
+        return name;
+    }
+        
+    public boolean spendPava(int amount) {
+        
+        if (pava <= 0) {
+         return false;
+        } else {
+            pava-=amount;
+            return true;
+
+        }
+    }
+    public static boolean receivePava() {
+        if (pava <= 0) {
+         return false;
+        } else {
+            pava +=amount
+            return true;
+
+        }
+    }
+    public int getPava(){
+        return pava;
+    }
+    public int getbirthYear(){
+        return birthYear;
+    }
+    public void setbirthYear(){
+        this.birthYear = birthYear;
+    }
+    public String toString(){
+        String stringRepresentation = name + "born in "  + String.valueOf(birthYear) + " has " + String.valueOf(pava) + " pava.";
+
+        return stringRepresentation;
+    }    
+    
+}/* See readme.md for what to do. */
 }
--- src/TavunuApp.java
+++ src/TavunuApp.java
@@ -11,9 +11,9 @@ public class TavunuApp {
      */
     public static void main(String[] args) {
         var t1 = new Tavunu();
-        var t2 = new Tavunu("Trast", 1988, 24);
+       // var t2 = new Tavunu("Trast", 1988, 24);
         System.out.println(t1);
-        System.out.println(t2);
+      //  System.out.println(t2);
     }
 
 }
